5ea7b3dc36f7983a19254275f8bde25bd5a69393
Santiago Larrain <slarrain@uchicago.edu>
2017-06-28 17:31:53 -0400

    Add delete

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pydkanGUI
 GUI implementation of pyDkan so agencies can easily upload files and create nodes
+
+It requires a forked version of pyDkan that accepts tokens.
+
+It uses [Gooey](https://github.com/chriskiehl/Gooey) for its implementation.
---
+++ dkan_gui.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+from gooey import Gooey
+from gooey import GooeyParser
+from dkan.client import DatasetAPI
+
+def run(args):
+    api = DatasetAPI(args.DkanHost, args.Token)
+    if args.Action == 'Create':
+        data = {
+          'title': args.title,
+          'type': args.type
+        }
+        print ("Attempting to create a new Dataset...")
+        dataset = api.node('create', data=data)
+        print("Process Completed: ", dataset.status_code)
+    elif args.Action == 'Attach_File':
+        print ("Attempting to Upload the file to the Dataset...")
+        r = api.attach_file_to_node(args.filename, args.node_id, 'field_upload')
+        print("Process Completed: ", r.status_code, r.text)
+    elif args.Action == 'Delete':
+        print ("Attempting to Delete ...")
+        op = api.node('delete', node_id=args.node_id)
+        print("Process Completed: ", op.status_code, op.text)
+
+    else:
+        print("We only support 'Create' and 'Upload' at the moment")
+
+@Gooey(advanced=True)
+def main():
+    parser = GooeyParser(description="DkanGUI")
+    parser.add_argument(
+        'DkanHost',
+        help="Url of the DKAN Host",
+        nargs='?',
+        )
+    parser.add_argument(
+        'Token',
+        help="Token to authorize you to DKAN"
+        )
+    parser.add_argument(
+        'Action',
+        help="Select the operation you want to perform:",
+        choices=['Create', 'Delete', 'Attach_File', 'Update']
+    )
+    parser.add_argument(
+        '--filename',
+        help="name of the file to Upload",
+        widget='FileChooser',
+        required=False,
+        )
+    parser.add_argument(
+        '--title',
+        help="Title of the Dataset you will be creating",
+        required=False,
+        )
+    parser.add_argument(
+        '--node_id',
+        type=int,
+        required=False,
+        help='Id of the Resource / Node / Dataset you are updating or\
+        uploading a file to'
+        )
+    parser.add_argument(
+        '--type',
+        help="Select the operation you want to perform:",
+        choices=['Dataset', 'Resource']
+        )
+
+    args = parser.parse_args()
+    run(args)
+
+
+if __name__ == '__main__':
+    main()
