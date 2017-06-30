e393f30c3359458a08636eb9b5e133f7c469f259
Santiago Larrain <slarrain@uchicago.edu>
2017-06-30 15:20:57 -0400

    Working version

--- dkan_gui.py
+++ dkan_gui.py
@@ -3,6 +3,7 @@ from gooey import Gooey
 from gooey import GooeyParser
 from dkan.client import DatasetAPI
 
+
 def run(args):
     api = DatasetAPI(args.DkanHost, args.Token)
     if args.Action == 'Create':
@@ -10,9 +11,9 @@ def run(args):
           'title': args.title,
           'type': args.type
         }
-        print ("Attempting to create a new Dataset...")
+        print ("Attempting to create a new Dataset/Resource...")
         dataset = api.node('create', data=data)
-        print("Process Completed: ", dataset.status_code)
+        print("Process Completed: ", dataset.status_code, dataset.text)
     elif args.Action == 'Attach_File':
         print ("Attempting to Upload the file to the Dataset...")
         r = api.attach_file_to_node(args.filename, args.node_id, 'field_upload')
@@ -21,9 +22,13 @@ def run(args):
         print ("Attempting to Delete ...")
         op = api.node('delete', node_id=args.node_id)
         print("Process Completed: ", op.status_code, op.text)
-
+    elif args.Action == 'Update':
+        print ("Attempting to Update ...")
+        r = api.node('update', node_id=int(args.node_id), data=eval(args.parameters))
+        print("Process Completed: ", r.status_code, r.text)
     else:
-        print("We only support 'Create' and 'Upload' at the moment")
+        print("We only support 'Create', 'Upload', 'Delete' and 'Update'\
+              at the moment")
 
 @Gooey(advanced=True)
 def main():
@@ -62,12 +67,13 @@ def main():
         )
     parser.add_argument(
         '--type',
-        help="Select the operation you want to perform:",
-        choices=['Dataset', 'Resource']
+        help="Select the Type of the Element",
+        choices=['dataset', 'resource']
         )
     parser.add_argument(
         '--parameters',
         help="Specify direct and explcit JSON to update",
+        type=str,
         required=False
         )
 
